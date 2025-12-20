9a14b4cd7eb14038996355c458477ae499913957
Tarannumm <tarannumelina@gmail.com>
2025-12-20 19:05:12 +0000

    final

--- lib/main.dart
+++ lib/main.dart
@@ -22,89 +22,93 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
         title: const Text(
           "Digital ID Card",
           style: TextStyle(
             color: Colors.white,
             fontWeight: FontWeight.bold,
-            fontSize: 28,
+            fontSize: 23,
           ),
         ),
-        backgroundColor: Color(0xff5a927e),
+        backgroundColor: Color(0xff3b5461),
         centerTitle: true,
       ),
       body: Center(
-        child: Stack(
-          alignment: Alignment.center,
-          children: [
-            // Card background
-            Container(
-              height: 400,
-              width: 300,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(16),
-                boxShadow: [
-                  BoxShadow(
-                    blurRadius: 5,
-                    color: Colors.black26,
+        child: Container(
+          padding: EdgeInsets.all(25),
+          margin: EdgeInsets.all(20),
+          width: 300,
+          decoration: BoxDecoration(
+            color: Color(0xfffbf4f4),
+            borderRadius: BorderRadius.circular(16),
+            boxShadow: [
+              BoxShadow(
+                blurRadius: 5,
+                color: Colors.black26,
+              ),
+            ],
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Container(
+                padding: EdgeInsets.all(3),
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  border: Border.all(color: Color(0xff878484), width: 3),
+                ),
+                child: CircleAvatar(
+                  radius: 45,
+                  backgroundImage: AssetImage("images/little-girl.jpg"),
+                ),
+              ),
+              SizedBox(height: 15),
+              Text(
+                "Anika Tarannum",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 5),
+              Text(
+                "Batch: 62nd, Department of CSE",
+                style: TextStyle(fontSize: 16, color: Colors.grey),
+              ),
+              SizedBox(height: 15),
+              Divider(),
+              SizedBox(height: 10),
+              Row(
+                children: [
+                  Icon(Icons.person_outlined, color: Colors.grey),
+                  SizedBox(width: 10),
+                  Text(
+                    "ID: 0182320012101301",
+                    style: TextStyle(fontSize: 16),
                   ),
                 ],
               ),
-            ),
-
-            // Content inside card
-            Column(
-              mainAxisSize: MainAxisSize.min,
-              children: const [
-                CircleAvatar(
-                  radius: 50,
-                  backgroundImage: AssetImage("images/little-girl.jpg"),
-                ),
-                SizedBox(height: 15),
-                Text(
-                  "Anika Tarannum",
-                  style: TextStyle(
-                    fontSize: 22,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                SizedBox(height: 8),
-                Text(
-                  "Batch: 62nd",
-                  style: TextStyle(
-                    fontSize: 16,
-                    color: Colors.grey,
-                  ),
-                ),
-                SizedBox(height: 5),
-                Text(
-                  "Department of CSE",
-                  style: TextStyle(
-                    fontSize: 16,
-                    color: Colors.grey,
-                  ),
-                ),
-                SizedBox(height: 8),
-                Text(
-                  "ID:01823200121301301",
-                  style: TextStyle(
-                    fontSize: 16,
-                    color: Colors.grey,
-                  ),
-                ),
-                SizedBox(height: 5),
-                Text(
-                  "phone:0175961****",
-                  style: TextStyle(
-                    fontSize: 16,
-                    color: Colors.grey,
-                  ),
-                ),
-              ],
-            ),
-          ],
+              SizedBox(height: 12),
+              Row(
+                children: [
+                  Icon(Icons.email_outlined, color: Colors.grey),
+                  SizedBox(width: 10),
+                  Text("Email: anika@gmail.com",
+                      style: TextStyle(fontSize: 16)),
+                ],
+              ),
+              SizedBox(height: 12),
+              Row(
+                children: [
+                  Icon(Icons.phone, color: Colors.grey),
+                  SizedBox(width: 10),
+                  Text("Phone: 0175961****", style: TextStyle(fontSize: 16)),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
